06892052ddc6750b78dc4893f0025bf32ce5ba5a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-06-30 10:11:45 +0530

    Added Firebase And Tested Using FCM Token

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
+import firebase from "./config/firebase";
+
 
 function App() {
+
+  useEffect( () => {
+      const messaging = firebase.messaging()
+      messaging.requestPermission().then(() => {
+          return messaging.getToken()
+      }).then(token => {
+        console.log(token)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>React Firebase Test</h1>
     </div>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBtgWEGKUit3hn7g8xdW9T9SzeCIfYmwz8",
+    authDomain: "react-firebase-94740.firebaseapp.com",
+    projectId: "react-firebase-94740",
+    storageBucket: "react-firebase-94740.appspot.com",
+    messagingSenderId: "308234876127",
+    appId: "1:308234876127:web:cda38b424a729326bca56a"
+}
+
+
+firebase.initializeApp(config)
+
+export default firebase
